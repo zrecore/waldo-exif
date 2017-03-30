044a083983cdd3c5aaac5b491f73e933818218d3
Alex Albino <webmaster@alexventure.com>
2017-03-30 00:27:13 -0700

    Wiring up basic dry run and processing request buttons

--- app/WaldoExifApp/src/app/app.component.ts
+++ app/WaldoExifApp/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EXIF Tool';
+  input_url = 'http://s3.amazonaws.com/waldo-recruiting';
+
+  dryRun(event) {
+    console.log("@TODO: Dry run!");
+  }
+
+  startProcessing(event) {
+    console.log("@TODO: Start processing!");
+  }
 }
